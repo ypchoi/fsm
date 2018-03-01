dbf0b1c7d184fe03ec5e83f10a77deddde7ddc74
AKer <sanybyme@hotmail.com>
2018-03-01 10:08:04 +0900

    * Initial commit

---
+++ demo/main.cpp
@@ -0,0 +1,8 @@
+﻿#include "../src/fsm_state.h"
+
+int main()
+{
+    FsmState<int> state(1);
+
+    return 0;
+}
---
+++ src/fsm_state.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+
+template <typename TType>
+class FsmState
+{
+public:
+    explicit FsmState(const TType& type)
+        : m_type(type)
+    {
+    }
+
+    TType GetType() const
+    {
+        return m_type;
+    }
+
+private:
+    TType m_type;
+};
